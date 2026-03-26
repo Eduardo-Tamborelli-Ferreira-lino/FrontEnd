3e3ef1377fc81c6820040d5b051678e8c81a8154
Eduardo Lino <eduardo_lino@estudante.sesisenai.org.br>
2026-03-26 14:18:06 -0300

    Dia 26/03: Um Pouco De CSS, HTML e JavaScript

---
+++ Estudo_JS/script.js
@@ -0,0 +1,24 @@
+var variavel = 79;
+console.log(typeof(variavel));
+variavel = "MI79";
+console.log(typeof(variavel))
+
+// Const
+const variavelConst = 79;
+console.log(typeof(variavelConst));
+variavelConst = "MI79";
+console.log(typeof(variavelConst))
+
+// Array
+const variavelArray = [];
+console.log(typeof(variavelArray));
+variavelArray = [];
+console.log(typeof(variavelArray));
+
+switch (variavel){
+    case "MI79":{
+        console.log(variavel)
+    } 
+    default :
+        console.log("da n")
+}
