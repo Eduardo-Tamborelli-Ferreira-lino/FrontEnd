602e2b0073d857fc8e17f8bca716c9380db0fbce
Eduardo Lino <eduardo_lino@estudante.sesisenai.org.br>
2026-04-02 13:54:52 -0300

    Continuando os exercícios e formatação da Lista 2

---
+++ Estudo_JS/Lista-de-exercicio/lista-1.js
@@ -0,0 +1,273 @@
+const prompt = require('prompt-sync')();
+console.log(`1 - Questão 1
+2 - Questão 2
+3 - Questão 3
+4 - Questão 4
+5 - Questão 5
+6 - Questão 6
+7 - Questão 7
+8 - Questão 8
+9 - Questão 9
+10 - Questão 10
+11- Questão 11
+12 - Questão 12
+13 - Questão 13
+14 - Questão 14
+0 - Sair
+`)
+let opcao = parseInt(prompt())
+switch (opcao) {
+    case 1: {
+        questao1()
+        break
+    }
+    case 2: {
+        questao2()
+        break
+    }
+    case 3: {
+        questao3()
+        break
+    }
+    case 4: {
+        questao4()
+        break
+    }
+    case 5: {
+        questao5()
+        break
+    }
+    case 6: {
+        questao6()
+        break
+    }
+    case 7: {
+        questao7()
+        break
+    }
+    case 8: {
+        questao8()
+        break
+    }
+    case 9: {
+        questao9()
+        break
+    }
+    case 10: {
+        questao10()
+        break
+    }
+    case 11: {
+        questao11()
+        break
+    }
+    case 12: {
+        questao12()
+        break
+    }
+    case 13: {
+        questao13()
+        break
+    }
+    case 14: {
+        questao14()
+        break
+    }
+    case 0: {
+        process.exit(0);
+    }
+}
+// Questão 1
+function questao1() {
+    let number = prompt("Insira O Número Que Você Deseja Verificar Se É Negativo Ou Positivo. ");
+    if (number === 0) {
+        console.log(`Seu Número é zero `);
+    }
+    else if (number < 0) {
+        console.log(`Seu Número ${number} é Negativo `);
+    }
+    else if (number > 0) {
+        console.log(`Seu Número ${number} é Positivo `);
+    }
+}
+
+
+// Questão 2
+function questao2() {
+    let soma = 0;
+    let notas;
+    let qtdNotas = parseInt(prompt(`Insira a quantidade de notas que você tem: `));
+    for (let i = 0; i < qtdNotas; i++) {
+        notas = parseInt(prompt(`Insira a ${i + 1}° nota: `));
+        soma += notas;
+    }
+    let media = soma / qtdNotas;
+    if (media < 3) {
+        console.log(`Você foi Reprovado Parabens`);
+    }
+    else if (media > 7) {
+        console.log(`Droga Você Foi Aprovado`);
+    }
+    else if (media >= 3 && media < 7) {
+        console.log(`Pegou Exame Lenda`);
+    }
+    else if (media == 10) {
+        console.log(`Tu é Bom, Aprovado com Excelência
+            Go Drinking `);
+    }
+}
+
+// Questão 3
+function questao3() {
+    let multiplos = new Array;
+    let number = 0;
+    do {
+        if (number % 3 === 0) {
+            multiplos.push(number);
+        }
+        number += 1;
+    } while (number <= 30)
+    console.log(`Os Multiplos de 3 são: `);
+    for (let i = 0; i < multiplos.length; i++) {
+        console.log(`${multiplos[i]}`);
+    }
+}
+
+// Questão 4
+function questao4() {
+    let number = parseInt(prompt(`Insira O Número que deseja vizualizar o fatorial `));
+    let soma = number;
+    if (number == 1 || number == 0) {
+        console.log(`O fatorial é 1`)
+        return
+    }
+    for (let i = (number - 1); i > 1; i--) {
+        soma *= i
+    }
+    console.log(`O fatorial é ${soma}`)
+}
+
+// Questão 5
+function questao5() {
+    let ano = Number(prompt("Insira o ano que deseja conferir se é bissexto ou não: "))
+    if (ano % 4 === 0 && ano % 100 != 0 || ano % 400 === 0) {
+        console.log(`O ano ${ano} é bissexto`)
+    }
+    else {
+        console.log(`O ano ${ano} não é bissexto`)
+    }
+}
+
+// Questão 6
+function questao6() {
+    let c = Number(prompt("Insira a temperatura em celsius: "))
+    let fahrenheit = (c * 9 / 5) + 32
+    console.log(`${c}° celsius é igual a ${fahrenheit}° fahrenheit`)
+}
+
+// Questão 7
+function questao7() {
+    let palavra = prompt("Insira Uma Palavra Para Verificar se Ela é ou Não um Palindromo: ").toLocaleLowerCase()
+    let invertida = palavra.split("").reverse().join("")
+    if (palavra == invertida) {
+        console.log(`A Palavra ${palavra} é um palindromo `)
+    }
+    else {
+        console.log(`A Palavra ${palavra} não é um palindromo `)
+    }
+}
+
+// Questão 8
+function questao8() {
+    let number = 1;
+    let soma = 0
+    do {
+        soma += number
+        number++
+    } while (number <= 100)
+    console.log(soma)
+}
+
+// Questão 9
+function questao9() {
+    let tabuada = []
+    let multiplicador = 1;
+    do {
+        tabuada.push(5 * multiplicador)
+        multiplicador++
+    } while (multiplicador <= 10)
+    console.log(tabuada)
+}
+
+// Questão 10
+function questao10() {
+    let number = 10
+    while (number > 0) {
+        console.log(number)
+        number--
+    }
+}
+
+// Questão 11
+function questao11() {
+    let number = 1
+    while (number <= 30) {
+        if (number % 3 === 0 && number % 5 === 0) {
+            console.log("FizzBuzz")
+        }
+        else if (number % 5 === 0) {
+            console.log("Buzz")
+        }
+        else if (number % 3 === 0) {
+            console.log("Fizz")
+        }
+        else {
+            console.log(number)
+        }
+        number++
+    }
+}
+
+// Questão 12
+function questao12() {
+    let number = 1;
+    let sequencia = ""
+    do {
+        if (number == 5) {
+            sequencia += number + "."
+        }
+        else {
+            sequencia += number + "-"
+        }
+        number++
+    } while (number <= 5)
+    console.log(sequencia)
+}
+
+// Questão 13
+function questao13() {
+    let linha = [" ", " ", " ", " ", "*", " ", " ", " ", " "]
+    let indexLeft = 3
+    let indexRight = 5
+    for (let i = 0; i <= 4; i++) {
+        let triangulo = ""
+        for (let j = 0; j < 9; j++) {
+            triangulo += linha[j]
+        }
+        console.log(triangulo)
+        linha[indexLeft] = ("*")
+        linha[indexRight] = ("*")
+        indexLeft--
+        indexRight++
+        if (i == 4) {
+            linha = [" ", " ", " ", " ", "*", " ", " ", " ", " "]
+        }
+    }
+}
+
+function questao14() {
+    let palavra = prompt("Insira uma Palavra: ")
+    let invertida = palavra.split("").reverse().join("")
+    console.log(palavra)
+    console.log(invertida)
+}
---
+++ Estudo_JS/Lista-de-exercicio/lista-2.js
@@ -0,0 +1,291 @@
+const prompt = require('prompt-sync')();
+
+// Arrows Functions
+let produto = (valor1, valor2) => valor1 * valor2;
+
+while (true) {
+    console.log(`
+    Lista - 2
+    --------------------
+    | 1 - Questão 1    |
+    | 2 - Questão 2    |
+    | 3 - Questão 3    |
+    | 4 - Questão 4    |
+    | 5 - Questão 5    |
+    | 6 - Questão 6    |
+    | 7 - Questão 7    |
+    | 8 - Questão 8    |
+    | 9 - Questão 9    |
+    | 10 - Questão 10  |
+    | 11- Questão 11   |
+    | 12 - Questão 12  |
+    | 13 - Questão 13  |
+    | 14 - Questão 14  |
+    | 0 - Sair         |
+    --------------------
+    `)
+    let opcao = parseInt(prompt())
+    switch (opcao) {
+        case 1: {
+            questao1()
+            break
+        }
+        case 2: {
+            questao2()
+            break
+        }
+        case 3: {
+            questao3()
+            break
+        }
+        case 4: {
+            questao4()
+            break
+        }
+        case 5: {
+            questao5()
+            break
+        }
+        case 6: {
+            questao6()
+            break
+        }
+        case 7: {
+            questao7()
+            break
+        }
+        case 8: {
+            questao8()
+            break
+        }
+        case 9: {
+            questao9()
+            break
+        }
+        case 10: {
+            questao10()
+            break
+        }
+        case 11: {
+            questao11()
+            break
+        }
+        case 12: {
+            questao12()
+            break
+        }
+        case 13: {
+            questao13()
+            break
+        }
+        case 14: {
+            questao14()
+            break
+        }
+        case 0: {
+            process.exit(0);
+        }
+    }
+}
+
+function questao1() {
+    let valor1 = Number(prompt("Insira O Primeiro Valor: "));
+    let valor2 = Number(prompt("Insira O Segundo Valor: "));
+    let soma = adicionar(valor1, valor2);
+    console.log(`\nA Soma Do Valor ${valor1} + ${valor2} = ${soma}`)
+}
+
+function adicionar(valor1, valor2) {
+    let soma = 0;
+    return soma = valor1 + valor2;
+}
+
+function questao2() {
+    let valor1 = Number(prompt("Insira O Primeiro Valor: "));
+    let valor2 = Number(prompt("Insira O Segundo Valor: "));
+    let diferenca = subtrair(valor1, valor2);
+    console.log(`\nA Diferença Do Valor ${valor1} + ${valor2} = ${diferenca}`)
+}
+
+function subtrair(valor1, valor2) {
+    let diferenca = 0;
+    return diferenca = valor1 - valor2;
+}
+
+
+function questao3() {
+    let valor1 = Number(prompt("Insira O Primeiro Valor: "));
+    let valor2 = Number(prompt("Insira O Segundo Valor: "));
+    console.log(`\nO Produto Dos Valores ${valor1} X ${valor2} = ${produto(valor1, valor2)}`);
+}
+
+
+function questao4() {
+    let numero = Number(prompt("Informe Um Número Para Ver Se Ele É Par: "));
+    if (ehPar(numero)) {
+        console.log(`O Número ${numero} é Par. `);
+        return;
+    }
+    console.log(`O Número ${numero} Não É Par. `)
+}
+
+function ehPar(numero) {
+    if (numero % 2 == 0) {
+        return true;
+    }
+    return false;
+}
+
+function questao5() {
+    let qtdNotas = Number(prompt("Quantas Notas Você Deseja Inserir?? "));
+    console.log()
+    let notas = [];
+    for (let i = 0; i < qtdNotas; i++) {
+        let nota = Number(prompt(`Qual a ${i + 1}° Nota: `));
+        console.log();
+        notas.push(nota);
+    }
+    let media = calcularMedia(notas) / qtdNotas;
+    console.log(`A Média Das Notas Inseridas é: ${media}`);
+}
+
+function calcularMedia(notas) {
+    let soma = 0;
+    for (let i = 0; i < notas.length; i++) {
+        soma += notas[i];
+    }
+    return soma;
+}
+
+function questao6() {
+    let palavra = prompt("Digite uma palavra sem letras maiusculas: ");
+    console.log();
+    console.log("Receba a mesma palavra só que tudo maisculo " + transformarString(palavra));
+}
+
+function transformarString(palavra) {
+    let PALAVRA = palavra.toLocaleUpperCase();
+    return PALAVRA;
+}
+
+function questao7() {
+    let limite = Number(prompt("Insira o Valor que Deseja Usar Como Limite: "));
+    let valores = [];
+    let qtdValores = Number(prompt("Informe a quantidade de valores que deseja inserir. "));
+    for (let i = 0; i < qtdValores; i++) {
+        let valor = Number(prompt(`Informe o ${i + 1}° valor: `));
+        valores.push(valor);
+    }
+    console.log(`Os números maiores que ${limite} são: `)
+    let maiores = maioresNumeros(valores, limite);
+    for (let i = 0; i < maiores.length; i++) {
+        console.log(maiores[i]);
+    }
+}
+
+function maioresNumeros(valores, limite) {
+    let maiores = [];
+    for (let i = 0; i < valores.length; i++) {
+        if (valores[i] > limite) {
+            maiores.push(valores[i]);
+        }
+    }
+    return maiores;
+}
+
+function questao8() {
+    let valor = Number(prompt("Informe o valor que deseja encontrar: "));
+    let numeros = [];
+    let qtdValores = Number(prompt("Informe a quantidade de valores que deseja inserir. "));
+    for (let i = 0; i < qtdValores; i++) {
+        let numero = Number(prompt(`Informe o ${i + 1}° número: `));
+        numeros.push(numero);
+    }
+    console.log(`\n O Número ${valor} se aparece : ${encontrarValor(numeros, valor)} dentro da lista. `)
+}
+
+function encontrarValor(numeros, valor) {
+    let quantidade = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] == valor) {
+            quantidade++;
+        }
+    }
+    return quantidade;
+}
+
+function questao9() {
+    console.log("Insira um texto que desejar fazer a contagem de caracterer.");
+    console.log(); j
+    let texto = prompt();
+    let quantidade = contarCaracter(texto)
+    console.log(`
+        Caracteres: ${quantidade[0]}
+        Espaços: ${quantidade[1]}
+        Total: ${quantidade[0] + quantidade[1]}`);
+}
+
+function contarCaracter(texto) {
+    let quantidade = 0;
+    let espacos = 0;
+    for (let i = 0; i < texto.length; i++) {
+        if (texto[i] != " ") {
+            quantidade++;
+        }
+        else {
+            espacos++;
+        }
+    }
+    let contagem = [];
+    contagem.push(quantidade);
+    contagem.push(espacos);
+    return contagem;
+}
+
+function questao10() {
+    console.log();
+    console.log("Vamos Iniciar um contagem no número 1. ");
+    console.log("Informe o ultimo da contagem. ");
+    console.log();
+    let numero = Number(prompt());
+    let contagem = contagemRegressiva(numero);
+    console.log(`Contagem Regressiva iniciando:`)
+    for (let i = 1; i < contagem.length; i++) {
+        console.log(contagem[i])
+        let inicio = Date.now();
+        while (Date.now() - inicio < 1000) {
+        }
+    }
+}
+
+function contagemRegressiva(numero) {
+    let contagem = [];
+    contagem.push(0);
+    for (let i = 1; i <= numero; i++) {
+        contagem.push(i);
+    }
+    return contagem;
+}
+
+function questao11() {
+
+}
+
+function questao12() {
+    console.log();
+    let numero = numeroAleatorio()
+    console.log(numero);
+    console.log();
+}
+
+function numeroAleatorio() {
+    return Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+}
+
+function questao13() {
+
+}
+
+function questao14() {
+
+}
