5c271e853d70e43d67dd054e8a8fe2d9fe447280
Eduardo Lino <eduardo_lino@estudante.sesisenai.org.br>
2026-04-14 14:02:06 -0300

    Finalização da Tier List: Adição de todos os campeões, correção de ícones (Wukong) e ajustes de estilo no footer.

--- TierList/script.js
+++ TierList/script.js
@@ -1,5 +1,6 @@
 const champions = document.querySelectorAll(".champion");
 const card = document.querySelectorAll(".champions-card");
+const filtersBtn = document.querySelectorAll(".filter-btn");
 
 champions.forEach(champion => {
     champion.addEventListener("dragstart", dragStart);
@@ -31,3 +32,33 @@ function dropElement(e) {
     const elementDrag = document.getElementById(id);
     e.currentTarget.appendChild(elementDrag);
 }
+
+
+filtersBtn.forEach(btn => {
+    btn.addEventListener("click", function () {
+        filtersBtn.forEach(b => b.classList.remove("active"));
+        this.classList.add("active");
+        const filterSelect = this.innerText.toLocaleLowerCase();
+        champions.forEach(champion => {
+            const roles = champion.getAttribute("data-role").toLocaleLowerCase();
+            if (filterSelect === "all" || filterSelect === "todos") {
+                champion.style.display = "flex";
+            }
+            else if (roles.includes(filterSelect)) {
+                champion.style.display = "flex";
+            }
+            else {
+                champion.style.display = "none";
+            }
+        });
+    })
+});
+
+window.addEventListener('dragover', (e) => {
+    const threshold = 100; // Distância da borda em pixels para começar a rolar
+    if (e.clientY < threshold) {
+        window.scrollBy(0, -10); // Sobe a página
+    } else if (window.innerHeight - e.clientY < threshold) {
+        window.scrollBy(0, 10); // Desce a página
+    }
+});
